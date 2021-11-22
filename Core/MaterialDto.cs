ff1693e887cb0f97da90db36bf1f031efe840612
Christoffer Gram <chkg@itu.dk>
2021-11-22 12:23:26 +0100

    Fix: Add Missing Options + Change Tag & MediaType to Dtos (#5)
    * Remove MediaType + Tag Enums
    
    * Add MediaType + Tag Dtos
    
    * Add Option + Naming Properties in UserDto
    
    * Remove WeatherForecast
    
    * Change UserDto Tags Prop. To Interests
    
    * Remove WeatherForecast Reference in FetchData
    
    * Use Strings in ICollections of Dtos
    
    * Add Reference to Rasmus's Repo

--- Core/MaterialDto.cs
+++ Core/MaterialDto.cs
@@ -1,4 +1,4 @@
 namespace blueberry.Core;
 
-public record MaterialDto(int id, string title, ICollection<Tag> tags);
-public record MaterialDetailsDto(int id, string title, ICollection<Tag> tags, string shortDescription, string? imageUrl, MediaType Type, DateTime date) : MaterialDto(id, title, tags);
+public record MaterialDto(int Id, string Title, IReadOnlySet<string> Tags);
+public record MaterialDetailsDto(int Id, string Title, IReadOnlySet<string> Tags, string ShortDescription, string? ImageUrl, string Type, DateTime Date) : MaterialDto(Id, Title, Tags);
--- Core/UserDto.cs
+++ Core/UserDto.cs
@@ -1,5 +1,5 @@
 namespace blueberry.Core;
 
-public record UserDto(int id, string name, ICollection<Tag> tags);
-public record UserCreateDto(string name, ICollection<Tag> tags);
-public record UserUpdateDto(int id, string name, ICollection<Tag> tags) : UserCreateDto(name, tags);
+public record UserDto(int Id, string Name, IReadOnlySet<string> Interests);
+public record UserCreateDto(string Name, IReadOnlySet<string> Interests);
+public record UserUpdateDto(int Id, string Name, IReadOnlySet<string> Interests) : UserCreateDto(Name, Interests);
