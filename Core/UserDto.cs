4160982ea4814fce952802f1d72f014db7aa240f
Christoffer Gram <chkg@itu.dk>
2021-11-22 11:45:25 +0100

    Add Option + Naming Properties in UserDto

--- Core/UserDto.cs
+++ Core/UserDto.cs
@@ -1,5 +1,5 @@
 namespace blueberry.Core;
 
-public record UserDto(int id, string name, ICollection<TagDto> tags);
-public record UserCreateDto(string name, ICollection<TagDto> tags);
-public record UserUpdateDto(int id, string name, ICollection<TagDto> tags) : UserCreateDto(name, tags);
+public record UserDto(int Id, string Name, ICollection<TagDto> Tags);
+public record UserCreateDto(string Name, ICollection<TagDto> Tags);
+public record UserUpdateDto(int Id, string Name, ICollection<TagDto> Tags) : UserCreateDto(Name, Tags);
