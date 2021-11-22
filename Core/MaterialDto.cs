812167b657abb019aca8319fbe3a384eeacb3201
Christoffer Gram <chkg@itu.dk>
2021-11-22 11:39:28 +0100

    Add MediaType + Tag Dtos

--- Core/MaterialDto.cs
+++ Core/MaterialDto.cs
@@ -1,4 +1,4 @@
 namespace blueberry.Core;
 
-public record MaterialDto(int id, string title, ICollection<Tag> tags);
-public record MaterialDetailsDto(int id, string title, ICollection<Tag> tags, string shortDescription, string? imageUrl, MediaType Type, DateTime date) : MaterialDto(id, title, tags);
+public record MaterialDto(int Id, string Title, ICollection<TagDto> Tags);
+public record MaterialDetailsDto(int Id, string Title, ICollection<TagDto> Tags, string ShortDescription, string? ImageUrl, MediaTypeDto Type, DateTime Date) : MaterialDto(Id, Title, Tags);
--- Core/UserDto.cs
+++ Core/UserDto.cs
@@ -1,5 +1,5 @@
 namespace blueberry.Core;
 
-public record UserDto(int id, string name, ICollection<Tag> tags);
-public record UserCreateDto(string name, ICollection<Tag> tags);
-public record UserUpdateDto(int id, string name, ICollection<Tag> tags) : UserCreateDto(name, tags);
+public record UserDto(int id, string name, ICollection<TagDto> tags);
+public record UserCreateDto(string name, ICollection<TagDto> tags);
+public record UserUpdateDto(int id, string name, ICollection<TagDto> tags) : UserCreateDto(name, tags);
