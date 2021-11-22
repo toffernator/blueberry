3d0f101c402504c4c710ba4304464767d222b1e3
Christoffer Gram <chkg@itu.dk>
2021-11-22 13:40:56 +0100

    Feature/infrastructure (#6)
    * Initial commit
    
    * Add First BlueBerryContext Implementation
    
    Also add EF-Core package with nuGet.
    
    * Initial commit
    
    * WIP Implement Repositories
    
    * Add First BlueBerryContext Implementation
    
    Also add EF-Core package with nuGet.
    
    * WIP UserRepository + Refacotization
    
    * Infrastructure UserRepository
    
    * SaveChangesAsync refactor
    
    Co-authored-by: Kobo-coder <jakob.hjalgrim@gmail.com>

--- Core/IUserRepository.cs
+++ Core/IUserRepository.cs
@@ -3,7 +3,7 @@ namespace blueberry.Core;
 public interface IUserRepository
 {
     public Task<UserDto> Create(UserCreateDto user);
-    public Task<UserDto> Read(int id);
+    public Task<Option<UserDto>> Read(int id);
     public Task<Status> Update(UserUpdateDto user);
     public Task<Status> Delete(int id);
 }
