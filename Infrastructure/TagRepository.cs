d05da7a6d823654cabe85503a39c42375d8d9bd7
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2021-11-26 14:19:42 +0100

    Feature/tag repository (#20)
    * Template for tests
    
    * WIP TagRepository Tests + Setup
    
    * Remove Program to Build
    
    * TagRepositoryTests Setup
    
    * WIP TagRepository Implementation
    
    * Ensure Database Setup for Testing
    
    * Fix Many to Many Relationship
    
    Fixed many to many relationship between User and Tag
    
    * Fix to TagRepositoryTest
    
    * Add missing directive
    
    * WIP TagRepository Implementation
    
    * Ensure Database Setup for Testing
    
    * Fix Many to Many Relationship
    
    Fixed many to many relationship between User and Tag
    
    * Fix to TagRepositoryTest
    
    * Add missing directive
    
    * Fix test header
    
    * Fix Method Pascal Casing
    
    * Fix UserDto to not update name
    
    * Fix UserUpdateDto
    
    Co-authored-by: Christoffer Gram <chkg@itu.dk>

--- Infrastructure/TagRepository.cs
+++ Infrastructure/TagRepository.cs
@@ -9,28 +9,65 @@ public class TagRepository : ITagRepository
         _context = context;
     }
 
-    public Task<(Status, TagDto)> Create(TagCreateDto tag)
+    public async Task<(Status, TagDto)> Create(TagCreateDto tag)
     {
-        throw new NotImplementedException();
+        var conflict = await _context.Tags
+                        .Where(t => t.Name == tag.Name)
+                        .Select(t => new TagDto(t.Id, tag.Name))
+                        .FirstOrDefaultAsync();
+
+        if (conflict != null)
+        {
+            return (Conflict, conflict);
+        }
+
+        var entity = new Tag(tag.Name);
+        
+        _context.Tags.Add(entity);
+
+        await _context.SaveChangesAsync();
+
+        return (Created, new TagDto(entity.Id, entity.Name));
     }
 
-    public Task<Status> Delete(int id)
+    public async Task<Status> Delete(int id)
     {
-        throw new NotImplementedException();
+        var entity = await _context.Tags
+                            .Include(t => t.Users)
+                            .FirstOrDefaultAsync(t => t.Id == id);
+
+        if (entity == null)
+        {
+            return NotFound;
+        }
+
+        if (entity.Users.Any())
+        {
+            return Conflict;
+        }
+
+        _context.Tags.Remove(entity);
+        await _context.SaveChangesAsync();
+
+        return Deleted;
     }
 
-    public Task<Option<TagDto>> Read(int id)
+    public async Task<Option<TagDto>> Read(int id)
     {
-        throw new NotImplementedException();
+        var tag = from t in _context.Tags
+                            where t.Id == id
+                            select new TagDto(t.Id, t.Name);
+
+            return await tag.FirstOrDefaultAsync();
     }
 
-    public Task<IReadOnlyCollection<TagDto>> Read()
+    public async Task<IReadOnlyCollection<TagDto>> Read()
     {
-        throw new NotImplementedException();
-    }
+        var tags = (await _context.Tags
+                    .Select(t => new TagDto(t.Id, t.Name))
+                    .ToListAsync())
+                    .AsReadOnly();
 
-    Task<(Status, TagDto)> ITagRepository.Create(TagCreateDto tag)
-    {
-        throw new NotImplementedException();
+        return tags;
     }
 }
