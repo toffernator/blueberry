fa1bb1eba2f2da8e2702070316b508d205e2c0d2
Christoffer Gram <chkg@itu.dk>
2021-12-02 20:13:43 +0100

    Add Test + Functionality
    Fails because mockOptions contains a Collection property so the mocking
    behaviour does not map otherwise identical options to return the
    mocked results.

--- Server.Tests/MaterialControllerTests.cs
+++ Server.Tests/MaterialControllerTests.cs
@@ -2,8 +2,9 @@ namespace blueberry.Server.Tests.Controllers;
 
 public class MaterialControllerTests
 {
+
     [Fact]
-    public async Task GetGivenLectureTagDockerStartYear2021EndYear2022TypeVideoReturnsLecture16()
+    public async Task GetGivenLectureTagDockerStartYear2021EndYear2022TypeVideoOffset0Limit2ReturnsLecture10AndLecture16()
     {
         var logger = new Mock<ILogger<MaterialController>>();
         var repository = new Mock<IMaterialRepository>();
@@ -16,13 +17,18 @@ public class MaterialControllerTests
             EndDate = new DateTime(2022, 1, 1),
             Type = "Video"
         };
-        var expected = new [] {new MaterialDto(1, "Lecture 16", new [] {"Docker", "C#"})};
+        var expected = new [] 
+        {
+            new MaterialDto(1, "Lecture 10", new [] {"Docker", "C#"}),
+            new MaterialDto(2, "Lecture 16", new [] {"Docker", "C#"})
+        };
         repository.Setup(m => m.Search(mockOptions)).ReturnsAsync(expected);
         
         var controller = new MaterialController(logger.Object, repository.Object);
-        var response = await controller.Get("Lecture", new HashSet<string> {"Docker"}, 2021, 2022, "Video");
+        var response = await controller.Get("Lecture", new HashSet<string> {"Docker"}, 2021, 2022, "Video", 0, 2);
 
         var isEqual = MaterialsEquals(expected, response.Value);
-        Assert.True(true);
+        Assert.True(isEqual);
     }
+
 }
