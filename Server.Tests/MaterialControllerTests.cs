8d94b8dbbfcd4bafc68dee331c8d2e0b6a1157c1
Christoffer Gram <chkg@itu.dk>
2021-12-03 08:30:06 +0100

    Change To Theory

--- Server.Tests/MaterialControllerTests.cs
+++ Server.Tests/MaterialControllerTests.cs
@@ -3,35 +3,26 @@ namespace blueberry.Server.Tests.Controllers;
 public class MaterialControllerTests
 {
 
-    [Fact]
-    public async Task GetGivenLectureTagDockerStartYear2021EndYear2022TypeVideoOffset0Limit2ReturnsLecture10AndLecture16()
+    [Theory]
+    [InlineData(0, 2)]
+    [InlineData(0, 1)]
+    [InlineData(2, 1)]
+    public async Task GetGivenOptionsOffsetAndLimitReturnsRangeOfResults(int offset, int limit)
     {
         var logger = new Mock<ILogger<MaterialController>>();
         var repository = new Mock<IMaterialRepository>();
         
-        var mockOptions = new SearchOptions
-        {
-            SearchString = "Lecture",
-            Tags = new HashSet<string>() {"Docker"},
-            StartDate = new DateTime(2021, 1, 1),
-            EndDate = new DateTime(2022, 1, 1),
-            Type = "Video"
-        };
-        var expected = new [] 
+       var expected = new [] 
         {
             new MaterialDto(1, "Lecture 10", new [] {"Docker", "C#"}),
             new MaterialDto(2, "Lecture 16", new [] {"Docker", "C#"})
         };
         repository.Setup(m => m.Search(It.IsAny<SearchOptions>())).ReturnsAsync(expected);
-    
-    
-
         
         var controller = new MaterialController(logger.Object, repository.Object);
-        var response = await controller.Get("Lecture", new HashSet<string> {"Docker"}, 2021, 2022, "Video", 0, 2);
+        var response = await controller.Get("Lecture", new HashSet<string> {"Docker"}, 2021, 2022, "Video", offset, limit);
 
-        var isEqual = MaterialsEquals(expected, response.Value);
+        var isEqual = MaterialsEquals(expected.Skip(offset).Take(limit), response.Value);
         Assert.True(isEqual);
     }
-
 }
