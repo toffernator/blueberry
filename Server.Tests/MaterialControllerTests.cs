5fafa70ee6a057a386ccab2511cbc28a111449d1
Christoffer Gram <chkg@itu.dk>
2021-12-03 09:26:05 +0100

    Feature/material controller results paging (#43)
    * Add Test + Functionality
    
    * Use Any SearchOptions for Mocking Behaviour
    
    * Change To Theory

--- Server.Tests/MaterialControllerTests.cs
+++ Server.Tests/MaterialControllerTests.cs
@@ -2,27 +2,27 @@ namespace blueberry.Server.Tests.Controllers;
 
 public class MaterialControllerTests
 {
-    [Fact]
-    public async Task GetGivenLectureTagDockerStartYear2021EndYear2022TypeVideoReturnsLecture16()
+
+    [Theory]
+    [InlineData(0, 2)]
+    [InlineData(0, 1)]
+    [InlineData(2, 1)]
+    public async Task GetGivenOptionsOffsetAndLimitReturnsRangeOfResults(int offset, int limit)
     {
         var logger = new Mock<ILogger<MaterialController>>();
         var repository = new Mock<IMaterialRepository>();
         
-        var mockOptions = new SearchOptions
+       var expected = new [] 
         {
-            SearchString = "Lecture",
-            Tags = new HashSet<string>() {"Docker"},
-            StartDate = new DateTime(2021, 1, 1),
-            EndDate = new DateTime(2022, 1, 1),
-            Type = "Video"
+            new MaterialDto(1, "Lecture 10", new [] {"Docker", "C#"}),
+            new MaterialDto(2, "Lecture 16", new [] {"Docker", "C#"})
         };
-        var expected = new [] {new MaterialDto(1, "Lecture 16", new [] {"Docker", "C#"})};
-        repository.Setup(m => m.Search(mockOptions)).ReturnsAsync(expected);
+        repository.Setup(m => m.Search(It.IsAny<SearchOptions>())).ReturnsAsync(expected);
         
         var controller = new MaterialController(logger.Object, repository.Object);
-        var response = await controller.Get("Lecture", new HashSet<string> {"Docker"}, 2021, 2022, "Video");
+        var response = await controller.Get("Lecture", new HashSet<string> {"Docker"}, 2021, 2022, "Video", offset, limit);
 
-        var isEqual = MaterialsEquals(expected, response.Value);
-        Assert.True(true);
+        var isEqual = MaterialsEquals(expected.Skip(offset).Take(limit), response.Value);
+        Assert.True(isEqual);
     }
 }
