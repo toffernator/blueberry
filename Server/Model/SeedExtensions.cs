886d87dfc87d43666cdafbc88c9a4d466bd6914d
Emil Østergaard <emil.oestergaard@theorg.com>
2021-12-03 17:59:12 +0100

    Exten seeding function

--- Server/Model/SeedExtensions.cs
+++ Server/Model/SeedExtensions.cs
@@ -19,8 +19,8 @@ public static class SeedExtensions
 
         var react = new Tag { Name = "React" };
         var blazor = new Tag { Name = "Blazor" };
-        var bootstrap = new Tag{ Name = "Bootstrap" };
-        var framework = new Tag{ Name = "Framework" };
+        var bootstrap = new Tag { Name = "Bootstrap" };
+        var framework = new Tag { Name = "Framework" };
 
         if (!context.Tags.Any())
         {
@@ -32,11 +32,11 @@ public static class SeedExtensions
         if (!context.Materials.Any())
         {
             context.Materials.AddRange(
-            new Material { Title = "Introduction to React", Tags = new List<Tag>() { react, framework }, Date = new DateTime(2020, 4, 11) },
-            new Material { Title = "Introduction to Blazor", Tags = new List<Tag>() { blazor, framework }, Date = new DateTime(2021, 2, 8) },
-            new Material { Title = "Introduction to Bootstrap", Tags = new List<Tag>() { bootstrap, framework }, Date = new DateTime(2018, 11, 23) },
-            new Material { Title = "Using Blazor with Bootstrap", Tags = new List<Tag>() { blazor, bootstrap, framework }, Date = new DateTime(2021, 8, 29) },
-            new Material { Title = "Using React with Bootstrap", Tags = new List<Tag>() { react, bootstrap, framework }, Date = new DateTime(2019, 9, 4) }
+            new Material { Title = "Introduction to React", Tags = new List<Tag>() { react, framework }, Date = new DateTime(2020, 4, 11), Type = "Video", ShortDescription = "" },
+            new Material { Title = "Introduction to Blazor", Tags = new List<Tag>() { blazor, framework }, Date = new DateTime(2021, 2, 8), Type = "Article", ShortDescription = "" },
+            new Material { Title = "Introduction to Bootstrap", Tags = new List<Tag>() { bootstrap, framework }, Date = new DateTime(2018, 11, 23), Type = "Article", ShortDescription = "" },
+            new Material { Title = "Using Blazor with Bootstrap", Tags = new List<Tag>() { blazor, bootstrap, framework }, Date = new DateTime(2021, 8, 29), Type = "Article", ShortDescription = "" },
+            new Material { Title = "Using React with Bootstrap", Tags = new List<Tag>() { react, bootstrap, framework }, Date = new DateTime(2019, 9, 4), Type = "Video", ShortDescription = "" }
             );
         }
 
