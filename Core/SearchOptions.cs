de95e15cbd4968979f8adf96ff836b1f186843f8
Karl Bonde Torp <kart@itu.dk>
2021-12-09 22:20:01 +0100

    Remove tricksy mergeconflict marker

--- Core/SearchOptions.cs
+++ Core/SearchOptions.cs
@@ -1,7 +1,3 @@
 namespace blueberry.Core;
 
-<<<<<<< HEAD
-public record SearchOptions(string SearchString = "", HashSet<string>? Tags = null, string? Type = null, DateTime? StartDate = null, DateTime? EndDate = null);
-=======
 public record SearchOptions(string SearchString = "", PrimitiveSet<string>? Tags = null, string? Type = null, DateTime? StartDate = null, DateTime? EndDate = null);
->>>>>>> origin/master
