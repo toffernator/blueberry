635e8f5c899921d3d30cae04724166c692b1354f
Gregers Rørdam <gregers@rordam.dk>
2021-12-12 23:38:05 +0100

    Use CacheMap to cache search results

--- Infrastructure/SearchProxy.cs
+++ Infrastructure/SearchProxy.cs
@@ -3,19 +3,33 @@ namespace blueberry.Infrastructure;
 public class SearchProxy : ISearch
 {
     private readonly ISearch _search;
+    private readonly CacheMap<SearchOptions, IReadOnlyCollection<MaterialDto>> _optionsCache;
+    private readonly CacheMap<string, IReadOnlyCollection<MaterialDto>> _stringCache;
 
     public SearchProxy(IMaterialRepository repo)
     {
         _search = new SearchProvider(repo);
+        _optionsCache = new CacheMap<SearchOptions, IReadOnlyCollection<MaterialDto>>(10);
+        _stringCache = new CacheMap<string, IReadOnlyCollection<MaterialDto>>(10);
     }
 
-    public Task<IReadOnlyCollection<MaterialDto>> Search(string searchString)
+    public async Task<IReadOnlyCollection<MaterialDto>> Search(string searchString)
     {
-        return _search.Search(searchString);
+        if (searchString == null) throw new ArgumentException("Cannot search for null");
+        if (_stringCache.TryGetValue(searchString, out IReadOnlyCollection<MaterialDto>? cached))
+            return cached;
+        var result = await _search.Search(searchString);
+        _stringCache.Add(searchString, result);
+        return result;
     }
 
-    public Task<IReadOnlyCollection<MaterialDto>> Search(SearchOptions options)
+    public async Task<IReadOnlyCollection<MaterialDto>> Search(SearchOptions options)
     {
-        return _search.Search(options);
+        if (options == null) throw new ArgumentException("Cannot search for null");
+        if (_optionsCache.TryGetValue(options, out IReadOnlyCollection<MaterialDto>? cached))
+            return cached;
+        var result = await _search.Search(options);
+        _optionsCache.Add(options, result);
+        return result;
     }
 }
