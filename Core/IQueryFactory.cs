f6bfcebafd964adc87b909a9e4b9c6ebd0c3b50b
Christoffer Gram <chkg@itu.dk>
2021-12-16 16:03:19 +0100

    Remove IQueryFactory.cs (#131)

--- Core/IQueryFactory.cs
+++
@@ -1,6 +0,0 @@
-namespace blueberry.Core;
-
-public interface IQueryFactory
-{
-    public IQueryable<MaterialDetailsDto> Create(IEnumerable<string?> tags, string? keywords, DateTime? startDate, DateTime? endDate);
-}
