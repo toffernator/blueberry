89b3c314c55ace6cd88dde253860be6ceefb69d6
Gregers Rørdam <gregers@rordam.dk>
2021-12-20 10:09:08 +0100

    Add interface documentation

--- Core/ISearch.cs
+++ Core/ISearch.cs
@@ -1,8 +1,14 @@
 namespace blueberry.Core;
 
+/// <summary>Defines the interface for searching through a body of materials</summary>
 public interface ISearch
 {
+    /// <summary>Search for materials matching searchString</summary>
     public Task<IReadOnlyCollection<MaterialDto>> Search(string searchString);
+
+    /// <summary>Search for materials matching the options specified in the <c>SearchOptions</c> object given as <param>options</param>.</summary>
     public Task<IReadOnlyCollection<MaterialDto>> Search(SearchOptions options);
+
+    /// <summary>Search by <c>SearchOptions</c> and a user id, interests being fetched for the user</summary>
     public Task<IReadOnlyCollection<MaterialDto>> Search(SearchOptions options, int id);
 }
